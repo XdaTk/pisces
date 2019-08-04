0557cf628c7f5e5bdc1677e20b7f4114fd07f932
xdatk <fq19930317@gmail.com>
2019-08-04 18:33:56 +0800

    添加常用Demo

--- sample/spa/main.go
+++ sample/spa/main.go
@@ -13,7 +13,15 @@ func hello(p pisces.Context) error {
 func main() {
 	p := pisces.New()
 	p.Use(middleware.Gzip())
+	p.Use(middleware.RequestID())
 	p.Use(middleware.Logger())
-	p.GET("/", hello)
+	p.Use(middleware.StaticWithConfig(middleware.StaticConfig{
+		Root:    "static",
+		HTML5:   true,
+	}))
+
+	g := p.Group("/api")
+	g.GET("/hello", hello)
+
 	_ = p.Start(":1099")
 }
